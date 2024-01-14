61f6672c3eec83bfa081511227efd805aba51a9d
Serhii1727 <kravchenko087@ukr.net>
2024-01-14 17:57:58 -0500

    add action reducer AddCntact

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,11 +1,36 @@
 import { NavLink, Outlet } from 'react-router-dom';
+import css from './SharedLayout.module.css';
+import logo from '../../images/icon.svg';
+import cart from '../../images/cart.svg';
 
 const SharedLayout = () => {
   return (
     <>
-      <header>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/cart">Cart</NavLink>
+      <header className={css.header}>
+        <div className={css.container}>
+          <div className={css.navLogo}>
+            <NavLink to="/">
+              <div>
+                <img src={logo} alt="Logo Food Boutique" />
+                Food Boutique
+              </div>
+            </NavLink>
+          </div>
+
+          <ul>
+            <li>
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/cart">
+                <div>
+                  <img src={cart} alt="Logo Cart" />
+                  Cart
+                </div>
+              </NavLink>
+            </li>
+          </ul>
+        </div>
       </header>
       <Outlet />
       <footer>Footer</footer>
